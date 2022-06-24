4d24ebfadcb0be2a67ef6553f8b41c8468e64896
ademi <ademirson15@hotmail.com>
2022-06-24 22:30:37 +0200

    Adding Database Connection

--- frontend/src/service/AuthService.js
+++ frontend/src/service/AuthService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { BASE_URL } from "./config"
 
 const AuthService = (function () {
     const _signin = async (credentials) => {
         try {
-            const response = await axios.post("/api/auth/login", credentials);
+            const response = await axios.post(BASE_URL + "/api/auth/login", credentials);
 
             if (response && response.data) {
                 localStorage.setItem("user", JSON.stringify(response.data))
@@ -23,7 +24,7 @@ const AuthService = (function () {
 
     const _signup = async (credentials) => {
         try {
-            return await axios.post("/api/auth/signup", credentials);
+            return await axios.post(BASE_URL + "/api/auth/signup", credentials);
         } catch (error) {
             console.log(error);
         }
